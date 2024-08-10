c6e3d34b026bde0f176eefb10abf1502d8142a99
Vito Videtta <vvidetta@yahoo.co.uk>
2024-08-10 19:38:53 +0100

    Add query parameters to Welcome action

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,8 +13,8 @@ public class HelloWorldController : Controller
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    public string Welcome(string name, int numTimes = 1)
     {
-        return "This is the Welcome action method...";
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
     }
 }
